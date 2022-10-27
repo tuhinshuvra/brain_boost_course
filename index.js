df5ba0e6e8484b0e968441db81261e21791e24af
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-10-27 15:54:25 +0600

    updated server name to show

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('Brain food server in running.')
+    res.send('Brain Boost Course Server in running.')
 })
 
 app.get('/courses', (req, res) => {
@@ -22,5 +22,6 @@ app.get('/courses/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Brain food server is running on Port:', port);
+    console.log('Brain Boost Course Server is Running on Port:', port);
+    console.log('Thanks you! for using the server. ');
 })
